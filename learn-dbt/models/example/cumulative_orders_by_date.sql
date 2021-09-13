4d86cca44ef0696f10d033d474619c64396a6af6
Denys Soloviov <desol@ciklum.com>
2021-09-13 13:27:42 +0300

    more updates

--- learn-dbt/models/example/cumulative_orders_by_date.sql
+++ learn-dbt/models/example/cumulative_orders_by_date.sql
@@ -1,8 +1,15 @@
-SELECT
-    DISTINCT
-    O_ORDERDATE,
-    SUM(O_TOTALPRICE) OVER (ORDER BY O_ORDERDATE) as O_CUMULATIVESALES
-FROM
-    "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS"
-ORDER BY
-    O_ORDERDATE
+with orders as (
+  SELECT *
+  FROM {{source('sample', 'orders')}}
+)
+
+select distinct
+    o_orderdate,
+    sum(o_totalprice) over (order by o_orderdate) as cumulative_sales
+from orders
+
+{% if target.name == 'dev' %}
+where year(o_orderdate) = 1996
+{% endif %}
+
+order by o_orderdate
